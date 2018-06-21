2d20c43f4b9c2869b003243bc2eee249f3f48ab9
kongxianghuan <xianghuan.kong@gmail.com>
2018-06-21 18:07:21 +0800

    bind 实现

---
+++ origin/test/bind.test.js
@@ -0,0 +1,21 @@
+var foo = { value: 1 }
+
+function bar(name, age) {
+  this.habit = 'shopping'
+  this.name = name
+  this.age = age
+}
+bar.prototype.friend = 'kevin'
+
+var bindFoo = bar.bind(foo, 'daisy')
+
+test('Fn bind test', () => {
+  bindFoo(20)
+  expect(foo).toEqual({ value: 1, name: 'daisy', habit: 'shopping', age: 20 })
+})
+
+test('Fn binded as constructor', () => {
+  var obj1 = new bindFoo(18)
+  expect(obj1).toEqual({ name: 'daisy', habit: 'shopping', age: 18 })
+  expect(obj1.friend).toBe('kevin')
+})
